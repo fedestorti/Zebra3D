425baee43a3fd2403f54966a48945f161e4b4e62
Federico Storti <34277220+fedestorti@users.noreply.github.com>
2025-07-25 19:51:45 -0300

    Actualizar init.sql

--- init/init.sql
+++ init/init.sql
@@ -1,6 +1,7 @@
 -- 🧑‍💻 Tabla de usuarios
 CREATE TABLE usuarios (
     id_usuario SERIAL PRIMARY KEY,
+    apodo VARCHAR(100) NOT NULL UNIQUE,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
